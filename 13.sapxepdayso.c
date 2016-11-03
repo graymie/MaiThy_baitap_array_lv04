ebaaa18bc758efd39b48462c43ee4e48f1a4cd08
graymie <hoangtammaithy@gmail.com>
2016-11-03 10:25:43 +0700

    Add files via upload

---
+++ 13.sapxepdayso.c
@@ -0,0 +1,22 @@
+#include"stdio.h"
+void main()
+{
+int n,i,j,a[100],m;
+
+printf("nhap n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{ printf("nhap mang a %d",i);
+scanf("%d",&a[i]);
+ }
+ for(i=0;i<n-1;i++)
+ { for(j=i+1;j<n;j++)
+
+   {  if(a[i]>a[j])
+
+    { m=a[i];
+    a[i]=a[j];
+    a[i]=m;
+     }}}
+    for(i=0;i<n;i++)
+{ printf("%d",a[i]); } }
